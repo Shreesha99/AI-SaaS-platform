a232a8eacf2923ed861089062e34569b891c6705
Shreesha99 <shreeshavenkatram99@gmail.com>
2024-07-15 22:47:24 +0530

    Initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-teal-500",
+    bgColor: "bg-teal-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/music"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-cyan-500",
+    bgColor: "bg-cyan-500/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-lime-500",
+    bgColor: "bg-lime-500/10",
+    href: "/video"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-indigo-500",
+    bgColor: "bg-indigo-500/10",
+    href: "/code"
+  }
+]
+
+const DashboardPage = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI on <h1 className="text-yellow-500">HyperDrive</h1>
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          HyperIntelligent Conversations at <span className="text-red-500 font-italic">&lt;Warp&gt;</span> Speed
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+          onClick={() => router.push(tool.href)}
+          key={tool.href}
+          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5"/>
+          </Card>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default DashboardPage;
