20a354f2599296ab1c184aeec7fd134c3aeb741c
Shreesha99 <shreeshavenkatram99@gmail.com>
2024-07-19 18:00:57 +0530

    subtle ui changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ const AuthLayout = ({
     children: React.ReactNode
 }) => {
     return ( 
-        <div className="flex items-center justify-center h-full">
-            {children}
+        <div className="h-full bg-[#111827] overflow-auto">
+            <div className="mx-auto max-w-screen-xl h-full w-full">{children}</div>
         </div>
      );
 }
